bdb7198061a4dd869c5a6362728c0ef436b0b81c
michael-liedeman <michael.liedeman@ontariotechu.net>
2025-03-21 13:35:41 -0400

    completed test suite for User class

--- mirror/TestUser.java
+++ mirror/TestUser.java
@@ -1,3 +1,4 @@
+// Test Suite for User and Admin functions
 import static org.junit.Assert.assertEquals;
 
 import org.junit.*;
@@ -7,56 +8,61 @@ public class TestUser {
     String testPassword = "abc"; 
     int testLevel = 0;
 
+    //Test if a User Object can be created
     @Test
     public void testUserCreation() {
         User testUser = new User(testUsername,testPassword,testLevel);
     }
 
+    // Test that a User Objects properties are as expected
     @Test
     public void testUserProperties() {
 
     }
 
-    @Test
-    public void testUserRegister(){
-        
-    }
-
+    // Test that a User can Login to an account previously registered
     @Test
     public void testUserLogin() {
 
     }
 
+    //Test that a User can add a restaurant to a favourites list
     @Test
     public void testUserAddFavourites() {
 
     }
 
+    //Test that a User can remove a restaurant to a favourites list
     @Test
     public void testUserRemoveFavourites() {
 
     }
 
+    // Test that a User can leave a review for a restaurant
     @Test
     public void testUserAddReview(){
-        
+
     }
 
+    // Test that a User's access level to see if they can use admin level functions
     @Test
     public void testUserAccessLevel() {
 
     }
 
+    // Test an Admin user's ability to add a restaurant to the page
     @Test
     public void testAdminAddRestaurant() {
 
     }
 
+    // Test an Admin user's ability to remove a restaurant from the page
     @Test
     public void testAdminRemoveRestaurant() {
 
     }
 
+    // Test an Admin user's ability to update/edit a restaurant on the page
     @Test
     public void testAdminUpdateRestaurant() {
 
